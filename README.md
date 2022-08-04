5d89a9014a5c0ff73f01b63ec407b9c614cd7355
SaborCanela <thomas.garzon@est.ikiam.edu.ec>
2022-08-04 01:22:05 -0500

    pregunta 5

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
 # GBI6_ExamenPython
-- Thomas_Garzon
-- Quito
-
-| Caracteristicas de mi computadora| 
-| :---: |
-| sistema Operativo  Windows 10 |
-| Procesador Intel(R) Core(TM) i5-5300U CPU @ 2.30GHz   2.29 GHz | 
-| RAM utilizable  7,88 GB |
-| Identificador de dispositivo  A2FF5553-A8EB-4B01-BC8E-3444112381CE |
-
-Ananconda 	2021.11
-Modulo 'minigscience.py' version de python 3.10.5
-Version de biopython 1.65
+Thomas_Garzon
--- miningscience.py
+++ miningscience.py
@@ -57,6 +57,7 @@ def map_science(tipo):
             zip_count.append(zipcodes.count(z))
     import matplotlib.pyplot as plt
     #%matplotlib inline
+    fig = plt.figure()
     plt.scatter(zip_long, zip_lat, s = zip_count, c= zip_count)
     plt.colorbar()
 # only continental us without Alaska
@@ -79,4 +80,5 @@ def map_science(tipo):
     params = plt.gcf()
     plSize = params.get_size_inches()
     params.set_size_inches( (plSize[0] * 3, plSize[1] * 3) )
+    fig.savefig("img/mapas.jpg")
     return plt.show()
